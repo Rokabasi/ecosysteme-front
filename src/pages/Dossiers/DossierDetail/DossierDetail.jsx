ec6900e1d7ecb343a7733381a933f5bb9c65cad8
Bia nzambi <kabasiromain@gmail.com>
2025-09-16 18:15:45 +0100

    feat/romain:fix basic informations

--- src/pages/Dossiers/DossierDetail/DossierDetail.jsx
+++ src/pages/Dossiers/DossierDetail/DossierDetail.jsx
@@ -122,8 +122,8 @@ const DossierDetail = () => {
     
     const statusClasses = {
       'soumis': 'bg-blue-100 text-blue-800',
-      'approuve': 'bg-green-100 text-green-800',
-      'rejete': 'bg-red-100 text-red-800',
+      'validé': 'bg-green-100 text-green-800',
+      'rejeté': 'bg-red-100 text-red-800',
       'default': 'bg-gray-100 text-gray-800'
     };
     
